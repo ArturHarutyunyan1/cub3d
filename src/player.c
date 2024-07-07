8abaf1ae07bb4eefd178e06596324704fe60617b
= <=>
2024-07-07 20:32:54 +0400

    Fix rotation

--- src/player.c
+++ src/player.c
@@ -58,11 +58,11 @@ void rotate_left(t_game *game)
     double old_plane_x;
 
     old_dir_x = game->player.dir_x;
-    game->player.dir_x = game->player.dir_x * cos(-ROTATE_SPEED) - game->player.dir_y * sin(-ROTATE_SPEED);
-    game->player.dir_y = old_dir_x * sin(-ROTATE_SPEED) + game->player.dir_y * cos(-ROTATE_SPEED);
+    game->player.dir_x = game->player.dir_x * cos(ROTATE_SPEED) - game->player.dir_y * sin(ROTATE_SPEED);
+    game->player.dir_y = old_dir_x * sin(ROTATE_SPEED) + game->player.dir_y * cos(ROTATE_SPEED);
     old_plane_x = game->player.plane_x;
-    game->player.plane_x = game->player.plane_x * cos(-ROTATE_SPEED) - game->player.plane_y * sin(-ROTATE_SPEED);
-    game->player.plane_y = old_plane_x * sin(-ROTATE_SPEED) + game->player.plane_y * cos(-ROTATE_SPEED);
+    game->player.plane_x = game->player.plane_x * cos(ROTATE_SPEED) - game->player.plane_y * sin(ROTATE_SPEED);
+    game->player.plane_y = old_plane_x * sin(ROTATE_SPEED) + game->player.plane_y * cos(ROTATE_SPEED);
 }
 
 void rotate_right(t_game *game)
@@ -71,11 +71,11 @@ void rotate_right(t_game *game)
     double old_plane_x;
 
     old_dir_x = game->player.dir_x;
-    game->player.dir_x = game->player.dir_x * cos(ROTATE_SPEED) - game->player.dir_y * sin(ROTATE_SPEED);
-    game->player.dir_y = old_dir_x * sin(ROTATE_SPEED) + game->player.dir_y * cos(ROTATE_SPEED);
+    game->player.dir_x = game->player.dir_x * cos(-ROTATE_SPEED) - game->player.dir_y * sin(-ROTATE_SPEED);
+    game->player.dir_y = old_dir_x * sin(-ROTATE_SPEED) + game->player.dir_y * cos(-ROTATE_SPEED);
     old_plane_x = game->player.plane_x;
-    game->player.plane_x = game->player.plane_x * cos(ROTATE_SPEED) - game->player.plane_y * sin(ROTATE_SPEED);
-    game->player.plane_y = old_plane_x * sin(ROTATE_SPEED) + game->player.plane_y * cos(ROTATE_SPEED);
+    game->player.plane_x = game->player.plane_x * cos(-ROTATE_SPEED) - game->player.plane_y * sin(-ROTATE_SPEED);
+    game->player.plane_y = old_plane_x * sin(-ROTATE_SPEED) + game->player.plane_y * cos(-ROTATE_SPEED);
 }
 
 void update_player(t_game *game)
