cc511645c94238f2f31636c9badf132013b91787
= <=>
2024-07-10 01:18:07 +0400

    check map for spaces

--- src/validation.c
+++ src/validation.c
@@ -1,5 +1,42 @@
 #include "../include/cub.h"
 
+int	check(char s)
+{
+	if (s == ' ' || s == '\t' || s == '\r' || s == '\n')
+		return (1);
+	return (0);
+}
+
+void check_map(char **map)
+{
+    size_t i;
+    size_t j;
+
+    i = 1;
+	while (map[i])
+	{
+		j = 1;
+		if (j < ft_strlen(map[i]) - 1)
+		{
+			while (map[i][j + 1])
+			{
+				if ((j > ft_strlen(map[i]) - 1 || j > ft_strlen(map[i - 1]) - 1)
+					&& map[i][j] != '1' && check(map[i][j]) == 0)
+                    exit(printf("EROR\n"));
+				if (check(map[i][j]) == 1 || map[i][j] == '1')
+					j++;
+				else if (map[i][j] != '1' && check(map[i][j - 1]) == 0
+					&& check(map[i][j + 1]) == 0 && check(map[i + 1][j]) == 0
+					&& check(map[i - 1][j]) == 0)
+					j++;
+				else
+                    exit(printf("EROR\n"));
+			}
+		}
+		i++;
+	}
+}
+
 void validate(char *path)
 {
     t_game game;
@@ -7,8 +44,9 @@ void validate(char *path)
     if (!check_format(path))
         exit(printf("Error\nInvalid format\n"));
     game.map.grid = read_map(path);
-    // if (!surrounded_by_walls(&game))
-    //     ft_exit(&game, "Error\nMap is not surrounded by walls\n", 1);
+    check_map(game.map.grid);
+    if (!surrounded_by_walls(&game))
+        ft_exit(&game, "Error\nMap is not surrounded by walls\n", 1);
     if (!check_chars(&game))
         ft_exit(&game, "Error\nInvalid characters in map\n", 1);
 }
