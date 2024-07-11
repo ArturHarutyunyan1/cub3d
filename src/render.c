3dbdf49271ddbe8d0b4eef6d8e5e29e38a4b9aeb
= <=>
2024-07-12 02:27:15 +0400

    Norm

--- src/render.c
+++ src/render.c
@@ -6,11 +6,11 @@
 /*   By: arturhar <arturhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 02:21:15 by arturhar          #+#    #+#             */
-/*   Updated: 2024/07/12 02:21:16 by arturhar         ###   ########.fr       */
+/*   Updated: 2024/07/12 02:26:00 by arturhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../include/cub.h"
 
+#include "../include/cub.h"
 void	draw_floor_and_ceiling(t_game *game)
 {
 	int	x;
@@ -51,6 +51,7 @@ void	clear_image(t_game *game)
 		while (x < WIDTH)
 		{
 			my_mlx_pixel_put(&game->img, x, y, 0x000000);
+			x++;
 		}
 		y++;
 	}
